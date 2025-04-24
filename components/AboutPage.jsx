18269d97fc778b2a266b78d51515a6d3dae70f2a
Ali <169173653+DavronRustamjonov@users.noreply.github.com>
2025-04-24 14:46:32 +0500

    Update AboutPage.jsx

--- components/AboutPage.jsx
+++ components/AboutPage.jsx
@@ -1,14 +1,11 @@
 import './about.css'
 
-
 export default function AboutPage() {
-
   window.scroll(0,0)
-
   return (
     <div className='about'>
       <div className="about-header">
-        <h1>Acerca de nosotros</h1>
+        <h1>Biz haqimizda</h1>
         <p className="lead">
           Somos una tienda en línea comprometida con ofrecer productos de alta calidad a precios competitivos.
         </p>
@@ -18,41 +15,41 @@ export default function AboutPage() {
         <div className="about-section">
           <h2>Nuestra historia</h2>
           <p>
-            Fundada en 2023, nuestra misión es proporcionar una experiencia de compra excepcional a nuestros clientes,
-            ofreciendo una amplia gama de productos y un servicio al cliente de primera clase. Comenzamos como una
-            pequeña tienda local y hemos crecido hasta convertirnos en una de las tiendas en línea más confiables del
-            mercado. Nuestro éxito se debe a nuestro compromiso inquebrantable con la satisfacción del cliente y la
-            calidad de nuestros productos.
+           2023 yilda tashkil etilgan bizning vazifamiz mijozlarimizga ajoyib xarid qilish tajribasini taqdim etishdir,
+          keng turdagi mahsulotlar va birinchi darajali mijozlarga xizmat ko'rsatishni taklif qiladi. sifatida boshladik
+         kichik mahalliy do'kon va biz dunyodagi eng ishonchli onlayn-do'konlardan biriga aylandik
+          bozor. Bizning muvaffaqiyatimiz mijozlar ehtiyojini qondirishga sodiqligimiz va
+           mahsulotlarimiz sifati.
           </p>
         </div>
 
         <div className="about-section">
-          <h2>Nuestros valores</h2>
+          <h2>Bizning qadriyatlarimiz</h2>
           <ul className="values-list">
-            <li>Calidad: Ofrecemos solo los mejores productos a nuestros clientes.</li>
-            <li>Integridad: Somos honestos y transparentes en todas nuestras operaciones.</li>
-            <li>Servicio al cliente: Nos esforzamos por superar las expectativas de nuestros clientes.</li>
-            <li>Innovación: Constantemente buscamos formas de mejorar y evolucionar.</li>
+            <li>Biz mijozlarimizga faqat eng yaxshi mahsulotlarni taklif qilamiz.</li>
+            <li>Halollik: Biz barcha operatsiyalarimizda halol va shaffofmiz.</li>
+            <li>Mijozlarga xizmat ko'rsatish: Biz mijozlarimiz kutganidan oshib ketishga intilamiz.</li>
+            <li>Innovatsiyalar: Biz doimo takomillashtirish va rivojlanish yo'llarini qidiramiz.</li>
           </ul>
         </div>
 
         <div className="about-section">
-          <h2>Nuestro equipo</h2>
+          <h2>Bizning jamoamiz</h2>
           <div className="row">
             <div className="col-md-4 team-member">
               <img src="./bb8c76bcb73cf00e7d4ab920447a365c.png" alt="Juan Pérez" />
-              <h3>Juan Pérez</h3>
-              <p>Fundador y CEO</p>
+              <h3>Polonchiyev Pustonchi</h3>
+              <p>Zo'r bola </p>
             </div>
             <div className="col-md-4 team-member">
               <img src="./50382765fd5648c7876d91cc37b27394.png" alt="María González" />
-              <h3>María González</h3>
-              <p>Directora de Operaciones</p>
+               <h3>Polonchiyev Pustonchi</h3>
+              <p>Zo'r bola </p>
             </div>
             <div className="col-md-4 team-member">
               <img src="./ed7055b68adec22bfa8a88d441e83e9a.png" alt="Carlos Rodríguez" />
-              <h3>Carlos Rodríguez</h3>
-              <p>Jefe de Servicio al Cliente</p>
+           <h3>Polonchiyev Pustonchi</h3>
+              <p>Zo'r bola </p>
             </div>
           </div>
         </div>
